4a4f717f513028b7fc243bb26ca5a7eb42922aca
oryzaconseva <161263449+oryzaconseva@users.noreply.github.com>
2025-05-19 14:48:12 +0700

    factory and seeders

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -3,6 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
@@ -16,8 +21,18 @@ class MovieFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()-> sentence(rand(3, 6));
+        $slug = Str::slug($title);
         return [
-            //
+            'title' => $title,
+            'slug' => $slug,
+            'synopsis' => fake()->paragraph(rand(5,  10)),
+            'category_id' => Category::inRandomOrder()->first(),
+            'year' => fake()->year(),
+            'actors' => fake()->name() . ', ' . fake()->name() . ', ' . fake()->name(),
+            'cover_image' => 'https://picsum.photos/seed/' . Str::random(10) . '/480/640',
+            'created_at' => now(),
+            'updated_at' => now()
         ];
     }
 }
